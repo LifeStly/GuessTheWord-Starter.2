0dd50ca39fd5f8ca42bbd8197ea08840b9d17349
LifeStly <45162527+LifeStly@users.noreply.github.com>
2020-08-02 18:48:34 +0700

    05.2 Task: Encapsulate the LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,6 +17,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -28,7 +29,10 @@ class GameViewModel : ViewModel() {
     // The current word
     val word = MutableLiveData<String>()
     // The current score
-    val score = MutableLiveData<Int>()
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -101,4 +105,5 @@ class GameViewModel : ViewModel() {
             }
 
     }
+
 }
